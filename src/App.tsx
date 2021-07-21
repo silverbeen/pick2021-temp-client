bc7c3f34a871200b853c8c6427539f433d27a87d
oajh04 <cloudtubekr@naver.com>
2021-07-21 19:07:13 +0900

    [Add] recoil root

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 
-import React from "react";
+import React, { Suspense } from "react";
+import { RecoilRoot } from "recoil";
 import "./App.css";
 import AttendanceDetail from "./components/attendance/attendanceDetail/AttendanceDetail";
 
 function App() {
-  return <AttendanceDetail />;
+  return (
+    <RecoilRoot>
+      <Suspense fallback={<div>loading</div>}>
+        <AttendanceDetail />
+      </Suspense>
+    </RecoilRoot>
+    
+  )
 }
 
 export default App;
