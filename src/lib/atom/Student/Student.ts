9bcf444e887c0b503ecb333a5535e36d33d37fc5
oajh04 <cloudtubekr@naver.com>
2021-07-22 01:17:17 +0900

    [Add] 주석 제거

--- src/lib/atom/Student/Student.ts
+++ src/lib/atom/Student/Student.ts
@@ -2,25 +2,18 @@ import axios from 'axios';
 import { atom, selectorFamily } from 'recoil'
 import attendance from '../../api/attendance';
 
-// export const StudentState = atom({
-//     key: 'studentStaet',
-//     default: []
-// });
 
 export const StudentSelect = selectorFamily({
+
     key: "studnet/get",
     get: (gubun: string) => async () => {
       if (!gubun) return "";
   
-      // attendance.getStudent(guhun)
-      //   .then((res) => {
-      //     console.log(res.data)
-      //       return res.data
-      //   })
-
       const { data } = await axios.get(
         `http://13.125.161.204:8081/student/${gubun}`
       );
+
+      console.log(data)
       return data;
     }
   
